e7f22cf0c6d94d4da8e22bf104128235f4da1772
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2024-08-12 13:54:54 +0000

    introduccion de java

--- 20242/Ejecutar.java
+++ 20242/Ejecutar.java
@@ -1,5 +1,23 @@
 public class Ejecutar {
     public static void main (String[] args){
     System.out.println("helloword");
+
+// comentario de una sola linea
+
+/*
+ * comentario
+ * de
+ * varias
+ * lineas
+ */
+
+/*
+ * tipos de datos en java
+ * numericos -->(enteros) int, long (reales),float,double
+ * logicos-->boolean
+ * cadena-->char, string
+ */
+
+
     }
 }
