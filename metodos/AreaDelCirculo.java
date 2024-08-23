e2e7eb11c41189429cfb0a93ee7f52ad301c3cae
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2024-08-23 21:46:50 +0000

    ejercicio 1

---
+++ metodos/AreaDelCirculo.java
@@ -0,0 +1,13 @@
+// ejercicio 2
+
+import java.util.Scanner;
+
+public class AreaDelCirculo {
+
+    double r;
+    final double PI= 3.1416;
+    double AreaDelCirculo;
+    Scanner scanner = new Scanner(System.in);
+   
+    
+}
