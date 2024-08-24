07c0b27655b1d7c263a7215871497eba89e1d3ec
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2024-08-24 01:02:09 +0000

    ejercicios de metodos

--- metodos/HorasDeParqueadero.java
+++ metodos/HorasDeParqueadero.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 
 public class HorasDeParqueadero {
     int hora;
-    double iva= 0.20;
-    final int valorH= 2;
+    double iva = 0.20;
+    final int valorH = 2;
     double pagoparquedaro;
-    Scanner scanner= new Scanner(System.in);
-     public void leerhorasYcalculopago(){
+    Scanner scanner = new Scanner(System.in);
+
+    public void leerhorasYcalculopago() {
 
         System.out.println("ingrese la cantidad de horas en el parqueadero");
-        hora=scanner.nextInt();
-        pagoparquedaro=((valorH*hora)*iva)+(valorH*hora);
+        hora = scanner.nextInt();
+        pagoparquedaro = ((valorH * hora) * iva) + (valorH * hora);
 
-        System.out.println("El valor a pagar del parqueadero es:"+ pagoparquedaro);
-     }
+        System.out.println("El valor a pagar del parqueadero es:" + pagoparquedaro);
+    }
 
-public static void main(String[] args) {
-    HorasDeParqueadero hor= new HorasDeParqueadero();
-    hor.leerhorasYcalculopago();
-}
+    public static void main(String[] args) {
+        HorasDeParqueadero hor = new HorasDeParqueadero();
+        hor.leerhorasYcalculopago();
+    }
 }
