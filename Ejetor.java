f503a79e6f68cbfb804290144475d71c6b6dd5aa
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2024-08-26 14:28:56 +0000

    ejercicio

---
+++ Ejetor.java
@@ -0,0 +1,11 @@
+
+
+
+    public class Ejetor {
+    public static void main(String[] args) {
+        Marcador objMarcador1 = new Marcador("charpie", "azul", 0.5, true);
+        objMarcador1.rayar();
+        System.out.println(objMarcador1.toString());
+    }
+}
+
