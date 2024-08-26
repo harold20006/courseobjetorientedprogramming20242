86dc1ca850e10f83ea2051c076487d9253763a62
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2024-08-26 13:48:22 +0000

    ejercicios con metodos

--- metodos/Persona.java
+++ metodos/Persona.java
@@ -1,13 +1,24 @@
 import java.util.Scanner;
 
 public class Persona {
-     String nombre;
-     int peso;
-     double altura;
-     int edad;
-     Scanner scanner = new Scanner(System.in);
+    String nombre;
+    int peso;
+    double altura;
+    int edad;
+    Scanner scanner = new Scanner(System.in);
 
-     public void leerDatos() {
+    // constructor
+    public Persona(String nombre, int peso, double altura, int edad) {
+        this.nombre = nombre;
+        this.peso = peso;
+        this.altura = altura;
+        this.edad = edad;
+    }
+
+    public Persona() {
+    }
+
+    public void leerDatos() {
         System.out.println("ingrese su nombre:");
         nombre = scanner.next();
         System.out.println("ingrese su peso:");
@@ -17,9 +28,10 @@ public class Persona {
         System.out.println("ingrese su edad:");
         edad = scanner.nextInt();
 
-     }
-     public static void main(String[] args) {
+    }
+
+    public static void main(String[] args) {
         Persona objper = new Persona();
         objper.leerDatos();
-     }
+    }
 }
