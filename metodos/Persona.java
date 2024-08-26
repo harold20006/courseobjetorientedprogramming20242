36bfbc8138943cd7c1ce0240d8d7ce3996474908
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2024-08-26 12:44:06 +0000

    metodos

---
+++ metodos/Persona.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Persona {
+    char nombre;
+    int peso;
+    double altura;
+    int edad;
+    Scanner scanner = new Scanner(System.in);
+
+    public void leerDatos() {
+        System.out.println("ingrese su nombre:");
+        nombre = scanner.next().charAt(0);
+        System.out.println("ingrese su peso:");
+        peso = scanner.nextInt();
+        System.out.println("ingrese su altura:");
+        altura = scanner.nextDouble();
+        System.out.println("ingrese su edad:");
+        edad = scanner.nextInt();
+
+    }
+    public static void main(String[] args) {
+        Persona objper = new Persona();
+        objper.leerDatos();
+    }
+}
