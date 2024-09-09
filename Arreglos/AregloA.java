deb1d604823d8c0bdd1233875445eb39e5b7c0c8
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2024-09-09 13:01:26 +0000

    Arreglos

--- Arreglos/AregloA.java
+++ Arreglos/AregloA.java
@@ -1,9 +1,8 @@
 public class AregloA {
 
     public static void main(String[] args) {
-        int[] arreglo = new int[5];
+        int[] arreglo = {8, 15, 4, 20, 30};
         for (int i = 0; i < arreglo.length; i++) {
-            arreglo[i] = i;
             System.out.println("arreglo["+i+"] = "+ arreglo[i]);
         }
     }
