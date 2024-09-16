e54067a5a2ea5c0540a687669ea48492d61357b3
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2024-09-16 14:41:29 +0000

    Herencia

--- Herencia/EjecutarHernecia.java
+++ Herencia/EjecutarHernecia.java
@@ -4,10 +4,17 @@ public class EjecutarHernecia {
         Trabajador objt2 = new Vendedor("Pedro", "Produccion", 2, 10.0);
         Trabajador objt3 = new Operario("Maria", "Produccion", 3, 100);
         Trabajador objt4 = new Operario("Luis", "Ventas", 4, 150);
-
+       /* 
        System.out.println("el salario del vendedor es: " + objt1.salario());
        System.out.println("el salario del vendedor es: " + objt2.salario());
        System.out.println("l salario del operario es: "+ objt3.salario());
        System.out.println("el salario del operario es: "+ objt4.salario());
+
+       */
+         Trabajador[] trabajadores = {objt1, objt2, objt3, objt4};
+            for (Trabajador trabajador : trabajadores) {
+                System.out.println(trabajador);
+                System.out.println("Salario: " + trabajador.salario());
+            }
     }
 }
