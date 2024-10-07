9f67e886c7af8c37a433e2e58e3e73beb1956ded
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2024-10-07 14:42:24 +0000

    CAMBIO EN LA FRMULA

--- trabajo1/Analista.java
+++ trabajo1/Analista.java
@@ -1,11 +1,22 @@
 package trabajo1;
 
 public class Analista  extends  Trabajador{
-     private final double salariointegral = 2000000;
+     private  double salariointegral;
+     private double lineasdecodigo;
+     private final double VALOR_LINEA_CODIGO = 1000;
 
-    public Analista(String nombre, int edad, String cargo, int salario, int id) {
+    public Analista(String nombre, int edad, String cargo, int salario, int id, double lineasdecodigo) {
         super(nombre, edad, cargo, salario, id);
+        this.lineasdecodigo = lineasdecodigo;
 
+    }
+
+    public double getLineasdecodigo() {
+        return lineasdecodigo;
+    }
+
+    public void setLineasdecodigo(double lineasdecodigo) {
+        this.lineasdecodigo = lineasdecodigo;
     }
 
 
@@ -14,7 +25,7 @@ public class Analista  extends  Trabajador{
     }
 
     public double salario() {
-        return salariointegral;
+        return salariointegral+(lineasdecodigo*VALOR_LINEA_CODIGO);
     }
 
     
--- trabajo1/Nomina.java
+++ trabajo1/Nomina.java
@@ -5,7 +5,7 @@ public class Nomina {
     public static void main(String[] args) {
         Trabajador[] trabajadores = new Trabajador[4];
         trabajadores[0] = new Operario("Juan", 25, "Operario", 0, 1, 10);
-        trabajadores[1] = new Analista("Pedro", 30, "Analista", 0, 2);
+        trabajadores[1] = new Analista("Pedro", 30, "Analista", 0, 2, 100);
         trabajadores[2] = new Consultor("Maria", 35, "Consultor", 0, 3, 500000);
         trabajadores[3] = new Operario("Luis", 40, "Operario", 0, 4, 20);
         
